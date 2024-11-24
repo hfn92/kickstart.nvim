23423d873ad2518cb2875e954a21ef6d230529c7
hfn92 <Fab.Hoersch@gmail.com>
2024-11-24 16:20:44 +0100

    update winshift

--- lua/plugins/winshift.lua
+++ lua/plugins/winshift.lua
@@ -1,3 +1,10 @@
+SetKeyBinds {
+  n = {
+    ["<leader>ws"] = { "<CMD>WinShift<CR>", "winshift shift" },
+    ["<leader>ww"] = { "<CMD>WinShift swap<CR>", "winshift shift swap" },
+  },
+}
+
 return {
   "sindrets/winshift.nvim",
   config = true,
